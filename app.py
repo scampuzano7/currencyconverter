9b796360346938197318638616463a04a60bb3ef
scampuzano7 <126427129+scampuzano7@users.noreply.github.com>
2023-10-25 09:28:22 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 # Dictionary for currency symbols based on selection
 operation_words = {
